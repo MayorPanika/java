d1e766106526fa8bdea9faaae4d8ffc54e2d4868
MayorPanika <63604490+MayorPanika@users.noreply.github.com>
2020-04-14 07:28:25 -0400

    Update Main.java

--- Ariphmetics/src/com/company/Main.java
+++ Ariphmetics/src/com/company/Main.java
@@ -5,14 +5,17 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        // write your code here
         Scanner scanner = new Scanner(System.in);
         int a = scanner.nextInt();
         int b = scanner.nextInt();
         int c = scanner.nextInt();
-        if (a * b == c) {
+        
+        if (a * b == c)
+        {
             System.out.println("No");
-        }else{
+        }
+        else
+        {
             System.out.println("Yes");
         }
     }
